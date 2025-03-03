459e6caaf43e85065b27e87e351c2e4043fa2b02
Siim-byte <beloimiis@gmail.com>
2025-03-03 11:42:05 +0200

    Add files via upload

--- TARpe24SQL_Siim.sql
+++ TARpe24SQL_Siim.sql
@@ -21,3 +21,96 @@ insert into Gender (Id, Gender)
 values (1, 'Female')
 insert into Gender (Id, Gender)
 values (3, 'Unknown')
+
+-- vaatame tabeli sisu
+select * from Gender
+
+
+-- teeme tebali Person
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
+
+--andmete sisestamine
+insert into Person (Id, Name, Email, GenderId)
+values (1, 'Superman', 's@s.com', 2)
+insert into Person (Id, Name, Email, GenderId)
+values (2, 'Wonderwoman', 'w@w.com', 1),
+(3, 'Batman', 'b@b.com', 2),
+(4, 'Aquaman', 'a@a.com', 2),
+(5, 'Catwoman', 'c@c.com', 1),
+(6, 'Antman', 'ant"ant.com', 2),
+(7, 'Spiderman', 'spider@s.com', 2),
+(9, NULL, NULL, 2)
+
+--soovime vaadata Person tabeli andmeid
+select * from Person
+
+--võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_Gender_FK
+foreign key (GenderId) references Gender(Id)
+
+-- kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla väärtust,
+-- siis see automaatselt sisestab sellele reale väärtuse 3 e nagu meil
+-- on unkown
+alter table Person
+add constraint DF_Persons_GenderId
+default 3 for GenderId
+
+--
+insert into Person (Id, Name, Email)
+values (11, 'Kalevipoeg', 'k@k.com')
+
+-- piirangu kustutamine
+alter table Person
+drop constraint DF_Persons_GenderId
+
+--lisame uue veeru
+alter table Person
+add Age nvarchar(10)
+
+--lisame nr piirangu vanuse sisestamisel
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and age < 155)
+
+--kustutame rea
+delete from Person where Id = 11
+
+select * from Person
+
+--kuidas uuendada andmeid
+update Person
+set Age = 50
+where Id = 4
+
+
+alter table Person
+add City nvarchar(50)
+
+--kõik, kes elavad Gothami linnas
+select * from Person where City = 'Gotham'
+--kõik, kes ei ela Gothamis
+select * from Person Where city != 'Gotham'
+--variant nr 2
+select * from person where City <> 'Gotham'
+
+--näitab teatud vanusega inimesi
+select * from Person where Age = 100 or Age = 35 or Age = 27
+select * from Person where Age in (100, 35, 25)
+
+-- näitab teatud vausevahemikus olevaid inimesi
+select * from Person where Age > 30
+select * from Person where Age between 22 and 50
+
+--Wildcard e näitab kõik g-tähega linnad
+select * from Person where City like 'g%'
+--kõik emailid, kus on @-märk emailis
+select * from Person where Email like '%@%'
+
+--näitab kõiki, kellel ei ole  @-märki emailis
+select * from Person where Email not like '%@%'
+
