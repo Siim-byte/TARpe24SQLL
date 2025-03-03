e552d39101e546a13e1a706ae8725c50a4eefc84
Siim-byte <beloimiis@gmail.com>
2025-03-03 09:39:51 +0200

    Add files via upload

---
+++ TARpe24SQL_Siim.sql
@@ -0,0 +1,23 @@
+--loome db
+create database TARpe24SQL
+
+-- db valimine 
+use TARpe24SQL
+
+--db kustutamine
+drop database TARpe24SQL
+
+-- tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine
+insert into Gender (Id, Gender)
+values (2, 'Male')
+insert into Gender (Id, Gender)
+values (1, 'Female')
+insert into Gender (Id, Gender)
+values (3, 'Unknown')
